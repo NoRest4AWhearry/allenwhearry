8254eec9fa8e497dd1e164f0241521b24c76ab8e
Allen <awhearry@gmail.com>
2016-10-22 11:32:20 -0400

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,6 @@ switch(process.env.npm_lifecycle_event) {
     		devtool: 'source-map',
     		output: {
     			path: PATHS.build,
-          publicPath: '/allenwhearry/',
     			filename: '[name].[chunkhash].js',
     			// This is used for require.ensure. The setup
     			// will work without but this is useful to set.
